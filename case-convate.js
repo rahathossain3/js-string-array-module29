9faa8fa5ebc884f56e049ab6a81d0f6c597db93a
rahathossain3 <rahathossain210@gmail.com>
2022-02-16 00:42:46 +0600

    uper & lower case

---
+++ case-convate.js
@@ -0,0 +1,10 @@
+const anthem = "Amr sonar Bangla Ami Tomay Valovasi";
+const search = 'valo';
+// .toLowerCase    //.toUpperCase
+const userInput = 'blACkPinK';
+const saverdUserName = 'blackPink';
+// console.log(userInput.toLocaleLowerCase())
+// console.log(saverdUserName.toLocaleLowerCase())
+if (userInput.toLowerCase() == saverdUserName.toLowerCase()) {
+    console.log("user exists");
+}
