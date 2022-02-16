d354731ebd3c37c56b140941cf6321c458b953f3
rahathossain3 <rahathossain210@gmail.com>
2022-02-16 09:28:20 +0600

    string search matched

--- string-search.js
+++ string-search.js
@@ -1,32 +1,52 @@
 const products = [
     'Dell hardcore i29 200GB laptop',
     'iphone 1TB camera flashlight Phone',
-    'yellow laptop with black camera',
-    '1X59 lanovo commercial yoga laptop',
+    'yellow laptop with black camera dell',
+    'Dell 1X59 lanovo commercial yoga laptop',
     'LG supernova laptop',
     'HTC low price Phone',
-    'purple color phone with Laptop'
+    'Dell purple color phone with Laptop'
 ];
-const searching = 'laptop'
+const searching = 'dell'
 
-// indexOf()
+// indexOf()        --//full string er sob chack kore 
 
 const output = [];
 for (const product of products) {
     if (product.toLowerCase().indexOf(searching.toLowerCase()) != -1) {
-        output.push(product);
+        // output.push(product);
     }
 }
 
 // console.log(output);
 
-//includes()
+//includes()        --//full string er sob chack kore 
 
 for (const product of products) {
     if (product.toLowerCase().includes(searching.toLowerCase())) {
-        output.push(product);
+        // output.push(product);
     }
 
 }
 
+
+
+//start with         --//full string er sudhu first word chack kore 
+
+for (const product of products) {
+    if (product.toLowerCase().startsWith(searching.toLowerCase())) {
+        // output.push(product);
+    }
+}
+
+
+
+
+// endWith       --//full string er sudhu Last word chack kore 
+
+for (const product of products) {
+    if (product.toLowerCase().endsWith(searching.toLowerCase())) {
+        output.push(product);
+    }
+}
 console.log(output);
